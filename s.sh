09e6d9cde77cc5e4b742e2a4d9da1753315266ec
ghourimarti <ghourimarti@gmail.com>
2025-03-19 07:20:21 +0000

    adding initial layout of python project

---
+++ s.sh
@@ -0,0 +1,17 @@
+clear
+virtualenv ~/.venv
+source ~/.venv/bin/activate
+make install
+make test
+make format
+make lint
+make all
+
+#
+git status
+git add *
+git commit -m "adding initial layout of python project"
+git push
+
+
+# use for 
